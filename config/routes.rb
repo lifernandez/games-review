7134011826a35d0a191033bb29d4a641737d99dc
Linda Fernandez <fernandez.linda.cristal@gmail.com>
2023-07-03 13:34:47 +0100

    created necessary models, views, and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'games#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :games, only: [:index, :show] do
+    post 'bookmarks', on: :member, to: 'bookmarks#create'
+  end
+
+  resources :users, only: [:new, :create]
+  resources :bookmarks, only: [:index]
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
